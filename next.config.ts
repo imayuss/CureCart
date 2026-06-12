215da85b24eefc54f2349f42cb4c7d9190a23782
Prince Agrawal <princeagrawal1013@gmail.com>
2026-06-12 17:04:12 +0530

    fix: added imagekit domain to next.config to unblock image rendering

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
